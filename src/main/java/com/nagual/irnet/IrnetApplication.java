a548373b0b004756530bb2f2adf3134f77bf4fab
AlexNagual <alexkseron@gmail.com>
2018-06-27 01:18:17 +0700

    Deploy

--- src/main/java/com/nagual/irnet/IrnetApplication.java
+++ src/main/java/com/nagual/irnet/IrnetApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IrnetApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(IrnetApplication.class, args);
     NNObject.getInstance().init();
+    SpringApplication.run(IrnetApplication.class, args);
   }
 }
